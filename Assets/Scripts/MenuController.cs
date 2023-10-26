bb824f36238a085507e928f2fec85c483a31d3d4
iguerrero02 <itzeljuliza@gmail.com>
2023-10-26 00:55:08 -0600

    MenuInicial

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void LoadNivel1(){
+        SceneManager.LoadScene("Escenario 1");
+    }
+ public void LoadNivel2(){
+        SceneManager.LoadScene("Escenario 2");
+    }
+
+    public void Salir(){
+        Application.Quit();
+    }
+}
