10788f834d645c8081682fd30a236198bc86e796
Fatima123 <fatimiruiz141@gmail.com>
2023-11-28 20:35:27 -0600

    Agregar demonios

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float velocidadMovimiento = 5.0f;
+    public float velocidadMovimiento = 20.0f;
     public float velocidadRotacion = 200.0f;
     private Animator anim;
     public float x, y;
